f79846e5e12ba63ba8ccf9885433c0eec3ae1713
张永鹏 <407423058zyp@gmail.com>
2017-04-14 23:47:41 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,14 @@
 class JobsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
    def index
-      @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+     @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
     end
 
    def show
@@ -10,10 +17,10 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
       if @job.is_hidden
         flash[:warning] = "This Job already archieved"
         redirect_to root_path
-      end 
+      end
    end
 
-    end
+
 
     def new
         @job = Job.new
